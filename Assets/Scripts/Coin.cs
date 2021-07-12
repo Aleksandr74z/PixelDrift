65ca25116077ecf7ea2bdfebc34536c6bfc23665
Aleksandr74z <79164978+Aleksandr74z@users.noreply.github.com>
2021-07-12 20:26:00 +0300

    Cleaning the code

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,21 +4,14 @@ public class Coin : MonoBehaviour
 {
     [SerializeField] private GameObject _particleCoin;
 
-    
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
         var car = collision.gameObject.GetComponent<Car>();
         if (car)
-        {
-            
+        {            
             Destroy(gameObject);
-
             Instantiate(_particleCoin, transform.position, transform.rotation);
-
-            Destroy(_particleCoin, 2f);
-
         }
     }
 }
